b0241d24b266eee412605c3bbf1d10c328e1d5f4
iam52 <tupac52@gmail.com>
2024-11-12 10:43:00 +0900

    commit

---
+++ src/main/java/com/hunmin/domain/deplytest/DeplyTestApplication.java
@@ -0,0 +1,13 @@
+package com.hunmin.domain.deplytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeplyTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeplyTestApplication.class, args);
+    }
+
+}
